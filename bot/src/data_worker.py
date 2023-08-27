6d65a2d61ac77fd3fb2e297b6bc57ed35e5269d7
Sasha Khinevich <hinevics@gmail.com>
2023-08-27 22:23:08 +0300

    ✨feat: add func for update data of user

--- bot/src/data_worker.py
+++ bot/src/data_worker.py
@@ -1,15 +1,39 @@
-"Модуль для работы с датой по "
+"Модуль для работы с датой"
 
-from config import PATH_DATA
+from typing import Any
+import csv
 
-
-def load_data():
-    pass
-
-
-def update_data():
-    pass
+from config import PATH_DATA_USER, PATH_DATA_USERACTION
 
 
-def get_data():
-    pass
+header_action = ["user_name", "action"]
+header_user_data = ["user_name", "flat_id", "like"]
+
+
+def update_action(data: list[tuple[Any]]):
+    """Функция для обновления данных по действиям user'a
+
+    Args:
+        data (list[tuple[Any]]): Набор действий
+    """
+
+    with open(PATH_DATA_USERACTION, mode="a", newline="") as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(header_action)
+
+        for row in data:
+            csvwriter.writerow(row)
+
+
+def update_user(data: list[tuple[Any]]):
+    """Функция для обновления данных по user
+
+    Args:
+        data (list[tuple[Any]]): Набор данных по предпочтению
+    """
+    with open(PATH_DATA_USER, mode="a", newline="") as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(header_user_data)
+
+        for row in data:
+            csvwriter.writerow(row)
