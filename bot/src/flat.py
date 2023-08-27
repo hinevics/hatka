74c6c22dc8afb277c80e74b6bca06359e5b4bf1e
Sasha Khinevich <hinevics@gmail.com>
2023-08-27 22:57:00 +0300

    ✨feat: update return data

--- bot/src/flat.py
+++ bot/src/flat.py
@@ -7,4 +7,11 @@ def get_test_flat():
     with open(PATH_TEST_FLAT, mode='rb') as file:
         data = pickle.load(file)
     k = list(data.keys())
-    return data[k[random.randint(0, len(k) - 1)]]
+    flat_id = k[random.randint(0, len(k) - 1)]
+
+    return {
+        "id": flat_id,
+        "title": data["title"],
+        "href": data["href"],
+        "adres": data["adres"]
+    }
